093a7378b67f36723b5f76c878fc1784c4b51994
raul <raul@zamacona.me>
2014-06-05 10:18:39 -0500

    cambios header

--- branding/salvalana.php
+++ branding/salvalana.php
@@ -5,7 +5,7 @@
 		<p>Diseño de identidad, tarjeta de débito, estados de cuenta, contratos, uniformes, stands promocionales, posters y flyers.</p>
 	</div>
 	<figure>
-		<img class="block center" src="http://nodo.pw/amapola_web/wp/wp-content/uploads/albiceleste1.jpg" alt="Albiceleste">
+		<img class="block center" src="http://nodo.pw/amapola_web/wp/wp-content/uploads/salva1.jpg" alt="Salvalana">
 	</figure>
 	<span class="cerrar">x</span>
 </div>
